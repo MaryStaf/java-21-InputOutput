07ffc07ffe1320933b68e6fb0f2791adca078a87
MaryStaf <mariia.staforkina@gmail.com>
2017-12-06 20:57:51 +0200

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("/Users/mariiastaforkina/Desktop/File2.rtf");
+        InputStream inputStream = new FileInputStream(file);
+        OutputStream outputStream = System.out;
+        copy(inputStream, outputStream);
+
+
+    }
+
+    private static void copy(InputStream inputStream, OutputStream outputStream) throws IOException{
+        byte[] buff = new byte[64 * 1024];
+
+        while(inputStream.available() > 0){
+            int count = inputStream.read(buff);
+
+            outputStream.write(buff, 0, count);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+
+
+}
